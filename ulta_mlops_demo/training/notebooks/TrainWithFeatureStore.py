e563fc53d3c65026351f94531e0342f907dc6ec2
PatrickLeahey <PatrickLeahey>
2023-11-20 21:31:22 +0000

    Small change

--- ulta_mlops_demo/training/notebooks/TrainWithFeatureStore.py
+++ ulta_mlops_demo/training/notebooks/TrainWithFeatureStore.py
@@ -34,8 +34,8 @@ notebook_path =  '/Workspace/' + os.path.dirname(dbutils.notebook.entry_point.ge
 dbutils.library.restartPython()
 
 # COMMAND ----------
-# DBTITLE 1, Notebook arguments
 
+# DBTITLE 1, Notebook arguments
 # List of input args needed to run this notebook as a job.
 # Provide them via DB widgets or notebook arguments.
 
@@ -76,29 +76,29 @@ dbutils.widgets.text(
 )
 
 # COMMAND ----------
-# DBTITLE 1,Define input and output variables
 
+# DBTITLE 1,Define input and output variables
 input_table_path = dbutils.widgets.get("training_data_path")
 experiment_name = dbutils.widgets.get("experiment_name")
 model_name = dbutils.widgets.get("model_name")
 
 # COMMAND ----------
-# DBTITLE 1, Set experiment
 
+# DBTITLE 1, Set experiment
 import mlflow
 
 mlflow.set_experiment(experiment_name)
 mlflow.set_registry_uri('databricks-uc')
 
 # COMMAND ----------
-# DBTITLE 1, Load raw data
 
+# DBTITLE 1, Load raw data
 raw_data = spark.read.format("delta").load(input_table_path)
 raw_data.display()
 
 # COMMAND ----------
-# DBTITLE 1, Helper functions
 
+# DBTITLE 1, Helper functions
 from datetime import timedelta, timezone
 import math
 import mlflow.pyfunc
@@ -156,14 +156,14 @@ def get_latest_model_version(model_name):
 
 
 # COMMAND ----------
-# DBTITLE 1, Read taxi data for training
 
+# DBTITLE 1, Read taxi data for training
 taxi_data = rounded_taxi_data(raw_data)
 taxi_data.display()
 
 # COMMAND ----------
-# DBTITLE 1, Create FeatureLookups
 
+# DBTITLE 1, Create FeatureLookups
 from databricks.feature_store import FeatureLookup
 import mlflow
 
@@ -192,8 +192,8 @@ dropoff_feature_lookups = [
 ]
 
 # COMMAND ----------
-# DBTITLE 1, Create Training Dataset
 
+# DBTITLE 1, Create Training Dataset
 from databricks import feature_store
 
 # End any existing runs (in the case this notebook is being run for a second time)
@@ -230,8 +230,8 @@ training_df.display()
 # MAGIC Train a LightGBM model on the data returned by `TrainingSet.to_df`, then log the model with `FeatureStoreClient.log_model`. The model will be packaged with feature metadata.
 
 # COMMAND ----------
-# DBTITLE 1, Train model
 
+# DBTITLE 1, Train model
 import lightgbm as lgb
 from sklearn.model_selection import train_test_split
 import mlflow.lightgbm
@@ -260,9 +260,9 @@ num_rounds = 100
 model = lgb.train(param, train_lgb_dataset, num_rounds)
 
 # COMMAND ----------
-# DBTITLE 1, Log model and return output.
 
-# Log the trained model with MLflow and package it with feature lookup information.
+# DBTITLE 1, Log model and return output.
+# Log the trained model with MLflow and package it with feature lookup information
 fs.log_model(
     model,
     artifact_path="model_packaged",
@@ -271,7 +271,7 @@ fs.log_model(
     registered_model_name=model_name,
 )
 
-# The returned model URI is needed by the model deployment notebook.
+# The returned model URI is needed by the model deployment notebook
 model_version = get_latest_model_version(model_name)
 model_uri = f"models:/{model_name}/{model_version}"
 dbutils.jobs.taskValues.set("model_uri", model_uri)
